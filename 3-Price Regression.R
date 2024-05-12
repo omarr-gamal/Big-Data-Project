6aa864866e4c76931c4453119a4061a11001d56f
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2024-05-12 12:01:24 +0300

    fixed typo

--- 3-Price Regression.R
+++ 3-Price Regression.R
@@ -99,7 +99,7 @@ result <- run_linear_regression(
 # ------------------------------------------------------------------------------
 
 # Make predictions on the entire dataset
-houses$Predicted_Price <- predict(model, newdata = houses)
+houses$Predicted_Price <- predict(result$model, newdata = houses)
 
 
 # Scatterplot of Predicted vs. Actual Prices with perfect fit line
@@ -132,31 +132,7 @@ hist(
   xlim = c(-1000000, 3000000), ylim = c(0, 400)
 )
 hist(houses$Predicted_Price, col = rgb(0, 0, 0, alpha = 0.5), breaks = 100, add = TRUE)
-hist(residuals, col = rgb(1, 0, 0, alpha = 0.2), breaks = 100, add = TRUE)
+hist(residuals, col = rgb(1, 0, 0, alpha = 0.2), breaks = 200, add = TRUE)
 
 legend("topright", legend = c("Price", "Predicted Price", "Error"), fill = c("white", "black", "pink"))
-
-# --------------------------------------------------------------------
-
-
-# Calculate density for each value of Rooms
-density_data <- lapply(unique(houses$Rooms), function(room){
-  data <- subset(houses, Rooms == room)
-  density_values <- density(data$Price)
-  data.frame(Rooms = room, Price = density_values$x, Density = density_values$y)
-})
-
-density_data <- do.call(rbind, density_data)
-
-# Create ggplot
-ggplot(data = density_data, aes(x = Price, y = Density, color = as.factor(Rooms))) +
-  geom_line() +
-  labs(x = "Price", y = "Density", title = "Density of Price for Each Value of Rooms") +
-  theme_minimal()
-
-
-pairs(houses[c("Rooms", "Bedroom2", "Bathroom", "Car", "Landsize", 
-               "Distance", "h", "t", "u", "EMetropolitan", 
-               "EVictoria", "NMetropolitan", "NVictoria", "SEMetropolitan", 
-               "SMetropolitan", "WMetropolitan")])
 
